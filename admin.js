b1b5f9cf88125d4c5165eac348c3515394e66f95
ggutierrez1337 <105136455+ggutierrez1337@users.noreply.github.com>
2022-05-27 13:36:04 -0500

    Delete admin.js

--- admin.js
+++
@@ -1,5 +0,0 @@
-let sidebarToggle = document.querySelector(".sidebarToggle");
-sidebarToggle.addEventListener("click", function() {
-    document.querySelector("body").classList.toggle("active");
-    document.getElementById("sidebarToggle").classList.toggle("active")
-})
